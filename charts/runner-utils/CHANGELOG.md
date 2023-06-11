c8d10526bd53ea1848caaf3dcf1b79daea87960a
alfathmad24 <muhammad.ayub412@gmail.com>
2023-06-11 20:52:26 +0700

    update

---
+++ charts/runner-utils/CHANGELOG.md
@@ -0,0 +1,7 @@
+- v0.1.1
+
+Fix jenkins entrypoint should be Secret instead of ConfigMap
+
+- v0.1.0
+
+Initial init helm chart
--- custom-yaml/github-runners/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if runner is already configured
-if [ ! -f .runner ]; then
-  ./config.sh --url https://github.com/alfathmad24/nextjs-fe --token ALAQTRORRQ2IIVJCGNWEMSTEQSQ5S
-fi
-
-# Run the runner
-./run.sh
